55b84f74dcf4cbf7fc55dbc4a942f7fd9e8b7410
csr87 <74970184+csr87@users.noreply.github.com>
2020-11-26 17:53:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ conn.sendMessage(id, teks, MessageType.text)
 
 if (text.includes("!nulis")){
   const teks = text.replace(/!nulis /, "")
-axios.get(`https://mhankbarbar.herokuapp.com/nulis?text=${teks}&apiKey=zFuV88pxcIiCWuYlwg57`).then((res) => {
+axios.get(`https://salism3.pythonanywhere.com/nulis?text=${teks}&apiKey=zFuV88pxcIiCWuYlwg57`).then((res) => {
     let hasil = `Silahkan download hasil dibawah ini agar hasilnya lebih bagus! 👌\n\n${res.data.result}`;
     conn.sendMessage(id, hasil ,MessageType.text);
 })
