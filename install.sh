f4c3942997842fe760d07ae6c8569ff3b4c0e028
csr87 <74970184+csr87@users.noreply.github.com>
2021-04-08 15:32:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
-apt update
-apt upgrade
-apt install
-pkg update && pkg upgrade
-pkg install wget
-pkg install ffmpeg
-pkg install nodejs
+apt update -y && apt upgrade -y
+apt install git python wget -y
+apt install ffmpeg && apt install nodejs
+apt install npm
 npm i -g cwebp
 npm i -g ytdl 
 npm i
